76bcf04e253928c90ec4a79edaf746ad18e46978
Leonardo <leonardoagqz@gmail.com>
2020-03-23 18:50:53 -0300

    adicionado Decimal_para_Binario.pas para leitura do código

---
+++ Decimal_Binario/Decimal_para_Binario.pas
@@ -0,0 +1,22 @@
+program Decimal_para_Binario;
+
+var
+  decimal,x,resto:integer;
+  binario,strresto:string;
+  Begin
+    write ('Digite o valor decimal ');
+    read (decimal);
+    x:=decimal;
+    while (x>=2) do
+      begin
+        resto:=x mod 2;
+        str(resto,strresto);
+        binario:=strresto+binario;
+        x:=x div 2;
+      end;
+    str(x,strresto);
+    binario:=strresto+binario;
+    writeln ('O valor decimal ',decimal,' em binário é ',binario);
+    Readln;
+    Readln;
+  End.
