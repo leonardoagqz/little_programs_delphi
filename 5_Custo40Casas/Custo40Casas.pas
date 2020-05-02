924085f788499698edfaef4db31c5f3657adb6a5
Leonardo <leonardoagqz@gmail.com>
2020-05-02 16:24:14 -0300

    Desafio 06 6_Escrever numero por extenso

--- 5_Custo40Casas/Custo40Casas.pas
+++ 5_Custo40Casas/Custo40Casas.pas
@@ -28,19 +28,19 @@ begin
    A_quadrado  := L*L;
    A_triangulo := (L*L)/2;
    A_trapezio  := (((L*2)+L)/2)*(L*0.7);
-   A_circulo   := (PI*((diametroC/2)*2));
-   A_total     := (A_retangulo+A_quadrado+A_triangulo+A_trapezio+A_circulo);
+   A_circulo   := (PI*((diametroC/2)*(diametroC/2)));
+   A_total     := (A_retangulo+(A_quadrado*4)+A_triangulo+A_trapezio+(A_circulo*4));
 
    Writeln ('');
-   writeln ('Valor por metro quadrado: R$',V_pedreiro:2:2);
+   writeln ('Valor por metro quadrado: R$ ',FormatFloat('##,###,##0.00',V_pedreiro));
    writeln ('Área de um quarto: ',A_quadrado:2:2,' m2');
    writeln ('Área de um banheiro: ',A_circulo:2:2,' m2');
    writeln ('Área de um escritório: ',A_triangulo:2:2,' m2');
    writeln ('Área das salas: ',A_retangulo:2:2,' m2');
    writeln ('Área da copa e cozinha: ',A_trapezio:2:2,' m2');
    writeln ('Área de uma casa: ',A_total:2:2,' m2');
-   writeln ('Valor mão de obra de uma casa: R$',V_pedreiro*A_total:2:2);
-   writeln ('Valor mão de obra condomínio: R$',V_pedreiro*A_total*40:2:2);
+   writeln ('Valor mão de obra de uma casa: R$ ',FormatFloat('##,###,##0.00',V_pedreiro*A_total));
+   writeln ('Valor mão de obra condomínio: R$ ',FormatFloat('##,###,##0.00',V_pedreiro*A_total*40));
 
    Readln;
    Readln;
--- 6_Escrever numero por extenso/Escrever_numero_por_extenso.dpr
+++ 6_Escrever numero por extenso/Escrever_numero_por_extenso.dpr
@@ -1,5 +1,4 @@
 program Escrever_numero_por_extenso;
-// Escrever número por extenso de 0 a 10.000
 
 uses
   Winapi.Windows,
@@ -13,111 +12,144 @@ uses
   Vcl.Dialogs;
 
 var
-I, Cont: Integer;
-Num, u, d, c, m, x: string;
-valor : string;
+valorstr : string;
+valor, i: integer;
+m : integer;
+c : integer;
+d : integer;
+u : integer;
 
 begin
-  write('Iforme um número de 0 a 10.000: ');
-  Readln(valor);
-  I:= Length(valor);
-
-    if I = 1 then
+  write('Digite um valor entre 1 e 10.000: ');
+   readln (valorstr);
+  while True do
+  begin
+    while (StrToInt(valorstr) <0) or (StrToInt(valorstr) > 10000) do
     begin
-      case ( StrToInt(valor[1])) of
-        1 : write(' um ');
-        2 : write(' dois ');
-        3 : write(' tres ');
-        4 : write(' quatro ');
-        5 : write(' cinco ');
-        6 : write(' seis ');
-        7 : write(' sete ');
-        8 : write(' oito ');
-        9 : write(' nove ');
-     end;
-     end;
-     if I = 2 then
-    begin
-      case ( StrToInt(valor[1])) of
-        1 : write(' dez ');
-        2 : write(' vinte ');
-        3 : write(' trinta ');
-        4 : write(' quarenta ');
-        5 : write(' cinquenta ');
-        6 : write(' sessenta ');
-        7 : write(' setenta ');
-        8 : write(' oitenta ');
-        9 : write(' noventa ');
-     end;
+      Write('Erro: Informe um valor entre 0 e 10.000!!: ');
+      Readln(valorstr);
+    end;
 
-      case ( StrToInt(valor[2])) of
-        1 : write(' um ');
-        2 : write(' dois ');
-        3 : write(' tres ');
-        4 : write(' quatro ');
-        5 : write(' cinco ');
-        6 : write(' seis ');
-        7 : write(' sete ');
-        8 : write(' oito ');
-        9 : write(' nove ');
+
+    writeln('');
+    valor := StrToInt(valorstr);
+
+
+    m := (valor div 1000);
+    if (m=1) or (m=2)or (m=3)or(m=4)or (m=5)or(m=6)or (m=7)or(m=8)or(m=9)or(m=10) then
+    begin
+      begin
+        c := (StrToInt(valorstr[2]));
+        d := (StrToInt(valorstr[3]));
+        u := (StrToInt(valorstr[4]));
+      end;
+    end
+    else
+    c := (valor div 100);
+    d := (valor mod 100) div 10;
+    u := (valor mod 100) mod 10;
+
+    if (valor=0) then
+      write(' zero');
+
+    case ( m ) of
+      1 : begin
+            if ((c=0)and(d=0)and(u=0)) then
+              begin
+                write(' Um Mil ');
+              end
+              else
+              if ((c<>0)and(d<>0)and(u<>0)) then
+              begin
+                write('Mil');
+              end
+              else
+              if ((m=10)and(c=0)and(d=0)and(u=0)) then
+              write(' Dez mil ');
+          end;
+
+          2 : write(' Dois mil ');
+          3 : write(' Três mil ');
+          4 : write(' Quatro mil ');
+          5 : write(' Cinco mil ');
+          6 : write(' Seis mil ');
+          7 : write(' Sete mil ');
+          8 : write(' Oito mil ');
+          9 : write(' Nove mil ');
+    end;
+
+
+      case ( c ) of
+      1 : begin
+            if ((d=0)and(u=0)) then
+            write(' cem ')
+            else
+            write(' cento ');
+          end;
+          2 : write(' duzentos ');
+          3 : write(' trezentos ');
+          4 : write(' quatrocentos ');
+          5 : write(' quinhentos ');
+          6 : write(' seiscentos ');
+          7 : write(' setecentos ');
+          8 : write(' oitocentos ');
+          9 : write(' novecentos ');
       end;
 
-     end;
+      if ((c<>0)and(d<>0)) then
+      write('e');
 
-      if I = 3 then
-    begin
-      case ( StrToInt(valor[1])) of
-        1 : write(' cem ');
-        2 : write(' duzentos ');
-        3 : write(' tresentos ');
-        4 : write(' quatrocentos ');
-        5 : write(' quinheitos ');
-        6 : write(' seiscentos ');
-        7 : write(' setecentos ');
-        8 : write(' oitocentos ');
-        9 : write(' novecentos ');
-     end;
-
-
-
-     case ( StrToInt(valor[2])) of
-        1 : write(' dez ');
-        2 : write(' vinte ');
-        3 : write(' trinta ');
-        4 : write(' quarenta ');
-        5 : write(' cinquenta ');
-        6 : write(' sessenta ');
-        7 : write(' setenta ');
-        8 : write(' oitenta ');
-        9 : write(' noventa ');
-     end;
-
-
-
-     case ( StrToInt(valor[3])) of
-        1 : write(' um ');
-        2 : write(' dois ');
-        3 : write(' tres ');
-        4 : write(' quatro ');
-        5 : write(' cinco ');
-        6 : write(' seis ');
-        7 : write(' sete ');
-        8 : write(' oito ');
-        9 : write(' nove ');
+      case ( d ) of
+      1 : begin
+            case ( u ) of
+              0 : write(' dez ');
+              1 : write(' onze ');
+              2 : write(' doze ');
+              3 : write(' treze ');
+              4 : write(' quatorze ');
+              5 : write(' quinze ');
+              6 : write(' dezesseis ');
+              7 : write(' dezessete ');
+              8 : write(' dezoito ');
+              9 : write(' dezenove ');
+            end;
+          end;
+          2 : write(' vinte ');
+          3 : write(' trinta ');
+          4 : write(' quarenta ');
+          5 : write(' cinquenta ');
+          6 : write(' sessenta ');
+          7 : write(' setenta ');
+          8 : write(' oitenta ');
+          9 : write(' noventa ');
       end;
-     end;
+
+      if ( d <> 1 ) then
+      begin
+        if ( ((c<>0)or(d<>0)) and(u<>0)) then
+        write('e');
+
+        case ( u ) of
+          1 : writeln(' um ');
+          2 : writeln(' dois ');
+          3 : writeln(' três ');
+          4 : writeln(' quatro ');
+          5 : writeln(' cinco ');
+          6 : writeln(' seis ');
+          7 : writeln(' sete ');
+          8 : writeln(' oito ');
+          9 : writeln(' nove ');
+        end;
+
+         write('.......................................');
+      end;
+      writeln('');
+      writeln('');
+      write('Digite um valor entre 1 e 10.000: ');
+      readln (valorstr);
 
 
-
-
-
-
-
-
-
-  Readln;
-  Readln;
-
-
+  end;
 
 end.
+
